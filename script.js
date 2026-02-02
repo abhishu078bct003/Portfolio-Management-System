6d6c4abc45acb8968b0bc73f83edaee5230a4592
abhishu078bct003 <abhishu.078bct003@acem.edu.np>
2026-02-02 16:01:14 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const modal = document.getElementById("serviceModal");
+const modalTitle = document.getElementById("modalTitle");
+const closeModal = document.getElementById("closeModal");
+const serviceCards = document.querySelectorAll(".service-card");
+const form = document.getElementById("serviceForm");
+const modalBox = document.querySelector(".service-modal");
+
+serviceCards.forEach(card => {
+    card.addEventListener("click", () => {
+        modalTitle.textContent = card.querySelector("h3").textContent;
+        modal.style.display = "flex";
+        document.body.style.overflow = "hidden";
+    });
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    modalBox.classList.remove("flipped");
+    document.body.style.overflow = "auto";
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    modalBox.classList.add("flipped");
+
+    setTimeout(() => {
+        modal.style.display = "none";
+        modalBox.classList.remove("flipped");
+        form.reset();
+        document.body.style.overflow = "auto";
+    }, 1500);
+});
